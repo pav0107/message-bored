180b38e96402f99459db55e719f63369497af59b
Pav <pav0107@gmail.com>
2022-11-07 16:54:22 +0000

    login redirects

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,15 +1,32 @@
 import { FcGoogle } from 'react-icons/fc';
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { auth } from '../../utils/firebase';
+import { useRouter } from 'next/router';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useEffect } from 'react';
 
 export default function Login() {
+  const route = useRouter();
+  const [user, loading] = useAuthState(auth);
+
   // Sign in with Google
   const googleProvider = new GoogleAuthProvider();
   const GoogleLogin = async () => {
     try {
       const result = await signInWithPopup(auth, googleProvider);
-    } catch (error) {}
+      route.push('/');
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  useEffect(() => {
+    if (user) {
+      route.push('/');
+    } else {
+      console.log('login');
+    }
+  }, [user]);
 
   return (
     <div className="p-10 mt-32 text-gray-700 rounded-lg shadow-xl">
